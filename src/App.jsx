38ebb1b41c563f9d6882be3c06e651b732de6c62
Vyadin <silverlock99@gmail.com>
2024-11-21 18:24:22 -0800

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from '@supabase/supabase-js'
-
-import './App.css'
+import "./App.css";
 
 const supabaseURL = import.meta.env.VITE_DATABASE_URL;
 const supabaseKey = import.meta.env.VITE_DATABASE_KEY;
-const supabase = createClient(supabaseURL, supabaseKey)
+const supabase = createClient(supabaseURL, supabaseKey);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   async function getLocations() {
     const { data } = await supabase.from("locations").select();
@@ -19,46 +16,27 @@ function App() {
   }
 
   async function insertLocation() {
-    const { error } = await supabase.from("locations").insert({checkin_data: {
-      name: "Emma",
-      city: "San Clemente, CA",
-      longitude: 172.123901,
-      latitude: -42.1239018
-    }})
+    const { error } = await supabase.from("locations").insert({
+      checkin_data: {
+        name: "Emma",
+        city: "San Clemente, CA",
+        longitude: 172.123901,
+        latitude: -42.1239018,
+      },
+    });
 
     if (error) {
-      console.log(`Error: ${error.message}`)
+      console.log(`Error: ${error.message}`);
     }
   }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={
-          () => 
-          setCount((count) => count + 1)
-          }
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="main">
+      <Header />
+      <Map />
+      <FormArea />
+    </div>
+  );
 }
 
-export default App
+export default App;
