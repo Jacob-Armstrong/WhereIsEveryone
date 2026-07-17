f8736eb0c016456998b65b26d9893855eaf6a1df
Jacob Armstrong <29027100+Jacob-Armstrong@users.noreply.github.com>
2026-07-17 13:44:43 -0600

    ...and the reference to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import { Analytics } from "@vercel/analytics/next"
-
 
 import Header from "./components/header/Header.jsx";
 import Map from "./components/map/Map.jsx";
